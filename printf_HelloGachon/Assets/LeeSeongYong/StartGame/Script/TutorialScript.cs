4a9768cb7f4538cecbce6d0e86ed1d0c7fe04076
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-06-01 19:02:21 +0900

    Fix: tutroial Fix

--- printf_HelloGachon/Assets/LeeSeongYong/StartGame/Script/TutorialScript.cs
+++ printf_HelloGachon/Assets/LeeSeongYong/StartGame/Script/TutorialScript.cs
@@ -8,7 +8,7 @@ public class TutorialScript : MonoBehaviour
     public Text tutoText;
     public Image tuto;
     public Sprite[] tutoImage;
-    string[] tutorial={"게임시작에서 이름을 입력하고 게임을 시작할 수 있습니다.","스토리 진행방식으로 게임이 진행됩니다.","플레이어에게 능력치가 부여됩니다.","플레이어가 선택한 선택지에 따라서 능력치가 다르게 상승하거나 감소합니다.","플레이어가 직접 원하는 능력치를 상승시킬 수 있습니다.","능력치는 게임의 엔딩점수에 영향을 줍니다."};
+    string[] tutorial={"게임시작에서 이름을 입력하고 게임을 시작할 수 있습니다.","게임은 스토리를 따라 진행됩니다.", "가천대와 캐릭터 방에 있는 특정 오브젝트들과 상호작용 할 수 있습니다.", "가천대와 캐릭터 방에 있는 특정 오브젝트들과 상호작용 할 수 있습니다.", "플레이어가 선택한 선택지에 따라 능력치가 상승하거나 감소합니다.","플레이어가 직접 원하는 활동을 골라 캐릭터를 육성시킬 수 있습니다.","능력치는 게임의 엔딩점수에 영향을 줍니다.", "캐릭터를 잘 육성해서 좋은 성적을 얻어보세요!"};
     int tutoIndex=0;
     private void Start() {
         tutoText.text=tutorial[tutoIndex];
@@ -16,13 +16,16 @@ public class TutorialScript : MonoBehaviour
     }
     public void NextTutorial()
     {
-        if(tutoIndex<tutorial.Length-1){
+        if(tutoIndex==tutorial.Length-1) {
+            tutoIndex=0;
+            tuto.sprite=tutoImage[tutoIndex];
+            tutoText.text=tutorial[tutoIndex];
+        }
+        else if(tutoIndex<tutorial.Length-1){
             tutoIndex++;
             tuto.sprite=tutoImage[tutoIndex];
             tutoText.text=tutorial[tutoIndex];
-            
         }
-        
     }
     public void BeforeTutorial()
     {
